30d0720d6dae1fe1d7ab1edf1190e0d12626a71f
WYDCXHJLMWB <130367615+WYDCXHJLMWB@users.noreply.github.com>
2025-04-25 12:48:15 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -92,7 +92,7 @@ if page == "性能预测":
                 st.markdown("### 🎯 预测结果")
                 st.metric(label="极限氧指数 (LOI)", value=f"{prediction:.2f} %")
 
-elif page == "逆向设计":
+elif page == "配方建议":
     # 用户输入的目标 LOI 需要在10到40之间
     target_loi = st.number_input("🎯 请输入目标 LOI 值 (%)", value=20.0, step=0.1, min_value=10.0, max_value=40.0)
 
